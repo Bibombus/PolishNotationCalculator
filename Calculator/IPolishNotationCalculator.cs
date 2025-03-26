21a9eeaefb9d26aa5997d75cdf54dbb77a316b28
Егор <yegor.nikulin.03@mail.ru>
2025-03-26 16:49:35 +0300

    Add interface specification

---
+++ Calculator/IPolishNotationCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public interface IPolishNotationCalculator
+    {
+        /// <summary>Вычисляет выражение в польской нотации</summary>
+        /// <param name="expression">Например: "+ 1 2"</param>
+        /// <exception cref="ArgumentException">При невалидном выражении</exception>
+        double Calculate(string expression);
+    }
+}
